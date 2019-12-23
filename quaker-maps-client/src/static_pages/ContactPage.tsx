5c520178b67d7c40c0341039bbb4c11e0a611da3
Micah Bales <micahbales@gmail.com>
2019-12-23 13:46:32 -0800

    Update Meta Description, FAQ, and Add Dev Deploy Option

--- quaker-maps-client/src/static_pages/ContactPage.tsx
+++ quaker-maps-client/src/static_pages/ContactPage.tsx
@@ -6,12 +6,12 @@ export const ContactPage: React.FC = () => (
         
         <h2>Hold On!</h2>
 
-        <p>Your question may have already been answered! Please visit our <a href="/faq">FAQ section</a> to be sure.</p>
+        <p>Your question may have already been answered! Please visit our <a href="/frequently-asked-questions">FAQ section</a> to be sure.</p>
 
         <h2>In Beta</h2>
 
         <p>Quakermaps.com is not complete or exhaustive. We are still in the programming and testing stages of this project, and we know that some of the information is not fully accurate. Thanks for helping us to make Quaker Maps better.</p>
 
-        <p>Still have a question? Reach out to us via email at: quakermaps@fojf.org</p>
+        <p>Still have a question? Reach out to us via email at: info@quakermaps.org</p>
     </div>
 )
