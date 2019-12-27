80284dce4f4b3a5dace160e8c821a631f0b272c1
Micah Bales <micahbales@gmail.com>
2019-12-26 22:43:20 -0800

    Add Google Analytics Tracking
    This update adds React-GA - a library that facilitates Google Analytics tracking of page views.
    
    This should mean that every time a user views a page on Quaker Maps, Google Analytics is informed.

--- quaker-maps-client/src/index.tsx
+++ quaker-maps-client/src/index.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import ReactGA from 'react-ga'
 
+// Google Analytics keeps track of pages that users visit
+const trackingId: string | undefined = process.env.REACT_APP_GA_TRACKING_ID
+if (trackingId) {
+    ReactGA.initialize(trackingId)
+    ReactGA.pageview(window.location.pathname + window.location.search)
+}
+
+// Mount the App
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
