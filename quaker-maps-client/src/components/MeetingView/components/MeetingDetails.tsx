f786fe10c468f74b719800a79443ba8cad43ffab
Micah Bales <micahbales@gmail.com>
2023-12-08 09:38:24 -0800

    Update MeetingsView Display Fields
    * Add new display fields
    * Handle gracefully for fields that may display multiple values (e.g. Yearly Meeting)

--- quaker-maps-client/src/components/MeetingView/components/MeetingDetails.tsx
+++ quaker-maps-client/src/components/MeetingView/components/MeetingDetails.tsx
@@ -1,50 +1,93 @@
-import { Card, Link, createStyles, makeStyles, Theme } from '@material-ui/core'
-import React from 'react'
-import { Meeting } from '../../../types'
+import { Card, Link, createStyles, makeStyles, Theme } from "@material-ui/core";
+import React from "react";
+import { Meeting } from "../../../types";
 
 /**
  * MeetingDetails is a card that contains the details for an individual meeting
  */
 
 interface MeetingDetailsProps {
-    meeting: Meeting,
-    style?: object
-    title?: string
+  meeting: Meeting;
+  style?: object;
+  title?: string;
+}
+
+function handleMultiValueFields(multiValueArr: string[]): string {
+  return multiValueArr.reduce(
+    (acc, val, i, m) => acc + `${val + (m[i + 1] ? ", " : " ")}`,
+    " "
+  );
 }
 
 export const MeetingDetails: React.FC<MeetingDetailsProps> = ({
-   meeting,
-   style,
-   title
+  meeting,
+  style,
+  title,
 }) => {
-    const classes = useStyles()
-    return (
-        <Card style={{ ...style, padding: '10px' }}>
-            {title && (
-                <Link href={`/meeting/${meeting.slug}`}>
-                    <h2 className={classes.header}>{meeting.title}</h2>
-                </Link>
-            )}
-            <ul className={classes.list}>
-                <li>{meeting.city && meeting.state && `Location: ${meeting.address && meeting.address} in ${meeting.city}, ${meeting.state}`}</li>
-                <li>{meeting.worship_style && `Worship Style: ${meeting.worship_style}`}</li>
-                <li>{meeting.yearly_meeting && `Yearly Meeting: ${meeting.yearly_meeting}`}</li>
-                <li>{meeting.branch && `Branch: ${meeting.branch}`}</li>
-                <li>{meeting.website && 'Website: '}{meeting.website && <a href={'http://' + meeting.website} target="_blank" rel="noopener noreferrer">{meeting.website}</a>}</li>
-            </ul>
-        </Card>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Card style={{ ...style, padding: "10px" }}>
+      {title && (
+        <Link href={`/meeting/${meeting.slug}`}>
+          <h2 className={classes.header}>{meeting.title}</h2>
+        </Link>
+      )}
+      <ul className={classes.list}>
+        {meeting.city && meeting.state && (
+          <li>
+            Location: {meeting.address && meeting.address} in {meeting.city},{" "}
+            {meeting.state}
+          </li>
+        )}
+        {meeting.worship_time && <li>Worship Time: {meeting.worship_time}</li>}
+        {meeting.worship_style.length > 0 && (
+          <li>
+            Worship Style: {handleMultiValueFields(meeting.worship_style)}
+          </li>
+        )}
+        {meeting.yearly_meeting.length > 0 && (
+          <li>
+            Yearly Meeting: {handleMultiValueFields(meeting.yearly_meeting)}
+          </li>
+        )}
+        {meeting.branch.length > 0 && (
+          <li>
+            Branch:
+            {handleMultiValueFields(meeting.branch)}
+          </li>
+        )}
+        {meeting.accessibility.length > 0 && (
+          <li>
+            Accessibility: {handleMultiValueFields(meeting.accessibility)}
+          </li>
+        )}
+        {meeting.description && <li>Notes: {meeting.description}</li>}
+        {meeting.website && (
+          <li>
+            Website:{" "}
+            <a
+              href={"http://" + meeting.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {meeting.website}
+            </a>
+          </li>
+        )}
+      </ul>
+    </Card>
+  );
+};
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        header: {
-            fontSize: 24,
-            textAlign: 'center',
-        },
-        list: {
-            listStyleType: 'none',
-            padding: 4,
-        }
-    })
-)
+  createStyles({
+    header: {
+      fontSize: 24,
+      textAlign: "center",
+    },
+    list: {
+      listStyleType: "none",
+      padding: 4,
+    },
+  })
+);
